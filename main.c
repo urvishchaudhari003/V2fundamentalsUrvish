02e749864fe519a39cac903796321c9cce951bff
unknown <anjanachaudhary7878@gmail.com>
2023-12-04 17:38:12 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+
+#define _CRT_NO_SECURE_WARNINGS // Corrected typo in the macro name
+
+#include <stdio.h>
+#include "fundamentals.h" 
+
+int main(void)
+{ // Added int before main
+
+    fundamentals();
+
+    return 0;
+}
